b8aff34a5f465174c2a3fe47dc1484dd6c853c8c
袁智武 <zhiwuyuan@kugou.net>
2020-08-15 11:06:12 +0800

    完善代码

--- app/src/main/java/com/example/yuanzhiwu/kotlin/utils/AppUtils.kt
+++ app/src/main/java/com/example/yuanzhiwu/kotlin/utils/AppUtils.kt
@@ -3,5 +3,28 @@ package com.example.yuanzhiwu.kotlin.utils
 import android.content.Context
 
 fun getAppVersionCode(context: Context): Int {
-    return 1
+    return 1.sum(1)
 }
+
+// 方法名     方法类型       方法体
+val sum: Int.(Int) -> Int = { other -> plus(other) }
+
+class HTML {
+    fun body() {
+
+    }
+}
+
+/*
+fun html(init: HTML.()-> Unit): HTML {
+    val html: HTML();
+    html.init()
+    return html
+}*/
+val foo:(Int) -> Int = {bar -> bar * 2}
+
+val li = listOf(1, 2, 3, 4, 5)
+val liNew = li.map(foo)
+
+val foo2:(Int) ->Boolean = {bar -> bar == 2}
+val listNew = li.filter(foo2)
