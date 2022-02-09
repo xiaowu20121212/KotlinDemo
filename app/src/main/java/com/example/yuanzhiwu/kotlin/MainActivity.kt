cf36f9dff61f26a4c95dc05aa150f2c8565b4bf8
袁智武 <zhiwuyuan@kugou.net>
2022-02-09 20:52:29 +0800

    完善代码

--- app/src/main/java/com/example/yuanzhiwu/kotlin/MainActivity.kt
+++ app/src/main/java/com/example/yuanzhiwu/kotlin/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.yuanzhiwu.kotlin
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,9 +11,35 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         this.setArrayValue(arrayOf(1, 2, 2))
         this.setArrayValue(arrayOf(1.0f, 2.0f, 2.0f))
+        val sum = { x: Int, y: Int -> x + y }
+        sum(1, 3)
+        var localreturn = localreturn()
+
+        var findUsers = findUsers()
+        findUsers.receiver
+
     }
 
     fun <T : Number> setArrayValue(array: Array<T>) {
 
+    }
+    private fun findUsers() = object {
+                val sender = 1
+                val receiver = 2
+            }
+
+
+    private fun localreturn(): Any {
+        val x = 1;
+        val y = 2;
+        var value = object {
+            val x1 = x
+            val y1 = y
+        }
+        return value
+    }
+
+    fun doFuntion(block:() -> Unit) {
+        block()
     }
 }
